dfee62246d463cca84d94028ba89ffd86d6e4fc8
Hirosi Miyata <sarena151413@gmail.com>
2018-06-19 18:22:06 +0900

    c1

--- sample/shop/shop_list.php
+++ sample/shop/shop_list.php
@@ -24,19 +24,34 @@ else
 <title>ろくまる農園</title>
 </head>
 <body>
+<?php
+require_once('../common/common.php');
+?>
+
+
+<form method="post" action="shop_list.php">
+<?php pulldown_santi(); ?>
+<?php pulldown_meigara(); ?>
+<?php pulldown_nedan(); ?>
+<p> 検索キーワードを入力
+<input type="search" name="keyword" value=""></p>
+<p><input type="submit" value="検索"></p>
+</form>
 
 <?php
 
 try
 {
 
-$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+require_once('../common/common.php');
+
+$dsn='mysql:dbname=test;host=localhost;charset=utf8';
 $user='root';
 $password='';
 $dbh=new PDO($dsn,$user,$password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-$sql='SELECT code,name,price FROM mst_product WHERE 1';
+$sql='SELECT code,name,price,santi,meigara,nedan FROM mst_product WHERE 1';
 $stmt=$dbh->prepare($sql);
 $stmt->execute();
 
@@ -44,6 +59,9 @@ $dbh=null;
 
 print '商品一覧<br /><br />';
 
+
+
+if($sql!==0){
 while(true)
 {
 	$rec=$stmt->fetch(PDO::FETCH_ASSOC);
@@ -51,12 +69,36 @@ while(true)
 	{
 		break;
 	}
+	if(isset($_POST['keyword']) && $_POST['keyword'] != '')
+{
+ $keyword = $_POST['keyword'];
+ $santi=$_POST['santi'];
+ $meigara=$_POST['meigara'];
+ $nedan=$_POST['nedan'];
+
+  if((strpos($rec['name'],$keyword)!==false)
+  &&(strpos($rec['santi'],$santi)!==false)
+	&&(strpos($rec['meigara'],$meigara)!==false)
+	&&(strpos($rec['nedan'],$nedan)!==false))
+  {
 	print '<a href="shop_product.php?procode='.$rec['code'].'">';
 	print $rec['name'].'---';
 	print $rec['price'].'円';
 	print '</a>';
 	print '<br />';
+	}
 }
+  else
+  {
+	print '<a href="shop_product.php?procode='.$rec['code'].'">';
+	print $rec['name'].'---';
+	print $rec['price'].'円';
+	print '</a>';
+	print '<br />';
+	}
+}
+}
+
 
 print '<br />';
 print '<a href="shop_cartlook.php">カートを見る</a><br />';
@@ -69,6 +111,7 @@ catch (Exception $e)
 }
 
 ?>
+
 
 </body>
 </html>
