8decbd75fbf15e3d43ff66ceb2d0903542e7c8a6
Hirosi Miyata <sarena151413@gmail.com>
2018-07-06 15:06:52 +0900

    c8

--- sample/product/pro_add_done.php
+++ sample/product/pro_add_done.php
@@ -39,12 +39,21 @@ $pro_santi=$post['santi'];
 $pro_meigara=$post['meigara'];
 $pro_nedan=$post['nedan'];
 
-$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
-$user='root';
-$password='';
-$dbh=new PDO($dsn,$user,$password);
-$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
+if (DEBUG) {
+	$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+	$user='root';
+	$password='';
+	$dbh=new PDO($dsn,$user,$password);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	}
+	else{
+	$dbServer = '127.0.0.1';
+	$dbUser = $_SERVER['MYSQL_USER'];
+	$dbPass = $_SERVER['MYSQL_PASSWORD'];
+	$dbName = $_SERVER['MYSQL_DB'];
+	$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
+	$dbh = new PDO($dsn, $dbUser, $dbPass);
+	}
 
 $sql='INSERT INTO mst_product(name,price,gazou,syousai,santi,meigara,nedan) VALUES (?,?,?,?,?,?,?)';
 $stmt=$dbh->prepare($sql);
